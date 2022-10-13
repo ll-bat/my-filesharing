cef34f529b41e99c1117508f35dd644e0c724cae
vladimer <gshatakishvili@gmail.com>
2022-10-13 23:55:42 +0400

    Update screensharing

--- live_streaming/client.py
+++ live_streaming/client.py
@@ -6,27 +6,44 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host_ip = '127.0.1.1'  # paste your server ip address here
 port = 9999
 client_socket.connect((host_ip, port))  # a tuple
-data = b""
-payload_size = struct.calcsize("Q") # Q: unsigned long long integer(8 bytes)
 
-#Business logic to receive data frames, and unpak it and de-serialize it and show video frame on client side
-while True:
-    while len(data) < payload_size:
-        packet = client_socket.recv(4 * 1024)  # 4K, range(1024 byte to 64KB)
-        if not packet: break
-        data += packet # append the data packet got from server into data variable
-    packed_msg_size = data[:payload_size] #will find the packed message size i.e. 8 byte, we packed on server side.
-    data=data[payload_size:] # Actual frame data
-    msg_size = struct.unpack("Q", packed_msg_size)[0] # meassage size
-    # print(msg_size)
+is_complete_image = False 
+image_data_bytes = b''
+image_data_bytes_list = []
+delimeter = pickle.dumps('@')
 
-    while len(data) < msg_size:
-        data += client_socket.recv(4 * 1024) # will receive all frame data from client socket
-    frame_data = data[:msg_size] #recover actual frame data
-    data=data[msg_size:]
-    image_data = pickle.loads(frame_data) # de-serialize bytes into actual frame type
-    cv2.imshow("RECEIVING VIDEO", image_data) # show video frame at client side
+while True:    
+    while is_complete_image is False:
+        received_data = client_socket.recv(4 * 1024)
+        if not received_data:
+            continue  
+        image_data_bytes_list.append(received_data)
+        if received_data.find(delimeter) >= 0:
+            is_complete_image = True 
+    
+    last_part = image_data_bytes_list.pop()
+    parts_list = last_part.split(delimeter)
+    image_data_bytes_list.append(parts_list[0])
+
+    try:
+        image_data = pickle.loads(b''.join(image_data_bytes_list)) # de-serialize bytes into actual frame type
+        print('pickled successfully')
+    except Exception as e:
+        print(image_data_bytes[-100:])
+        print(len(parts_list[0]))
+        print(str(e))
+        break 
+
+    cv2.imshow("RECEIVING VIDb';'EO", image_data) # show video frame at client side
+
+    image_data_bytes_list = [delimeter.join(parts_list[1:])]
+    if len(parts_list) > 2:
+        is_complete_image = True 
+    else:
+        is_complete_image = False 
+
     key = cv2.waitKey(1) & 0xFF
     if key == ord('q'): # press q to exit video
         break
+
 client_socket.close()
--- live_streaming/server.py
+++ live_streaming/server.py
@@ -4,6 +4,7 @@ import socket, cv2, pickle, struct, imutils
 import numpy as np
 from mss import mss
 from PIL import Image
+import json 
 
 # Socket Create
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -20,33 +21,44 @@ server_socket.bind(socket_address)
 server_socket.listen(1)
 print("LISTENING AT:", socket_address)
 
-mon = {'top': 0, 'left': 0, 'width': 100, 'height': 100}
 sct = mss()
+sct.compression_level = 15
+mon = sct.monitors[0]
 
 def get_image_data():
-    sct.get_pixels(mon)
-    img = Image.frombytes('RGB', (sct.width, sct.height), sct.image)
-    return np.array(img)
+    # Grab the data
+    sct_img = sct.grab(mon)
+    # sct.get_pixels(mon)
+    # img = Image.frombytes('RGB', (sct.width, sct.height), sct.image)
+    return np.array(sct_img)
 
 # Socket Accept
 while True:
     client_socket, addr = server_socket.accept()
-    print('GOT CONNECTION FROM:', addr)
-    if client_socket:
-        while True:
-            image_data = get_image_data()
-            
-            a = pickle.dumps(image_data) #serialize frame to bytes
-            message = struct.pack("Q", len(a)) + a # pack the serialized data
-            # print(message)
-            try:
-                client_socket.sendall(message) #send message or data frames to client
-            except Exception as e:
-                print(e)
-                raise Exception(e)
+    try:
+        print('GOT CONNECTION FROM:', addr)
+        if client_socket:
+            while True:
+                image_data = get_image_data()
 
+                image_bytes = pickle.dumps(image_data) #serialize frame to bytes
+                delimeter = pickle.dumps('@')
+                # print(message)
+                try:
+                    # client_socket.sendall(size_bytes)
+                    # client_socket.sendall(pickle.dumps(":"))
+                    print('sending imagewith size {}'.format(len(image_bytes)))
+                    client_socket.sendall(image_bytes + delimeter) #send message or data frames to client
+                except Exception as e:
+                    print(e)
+                    client_socket.close()
+                    break 
 
-            cv2.imshow('TRANSMITTING VIDEO', image_data) # will show video frame on server side.
-            key = cv2.waitKey(1) & 0xFF
-            if key == ord('q'):
-                client_socket.close()
+                # cv2.imshow('TRANSMITTING VIDEO', image_data) # will show video frame on server side.
+                # key = cv2.waitKey(1) & 0xFF
+                # if key == ord('q'):
+                #     client_socket.close()
+    except Exception as e:
+        pass 
+
+    client_socket.close()
